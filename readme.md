3a88c267a9a6cb7b3f11b58b42242c15a958fa9c
Leo-160905 <Leonardvonfircks@gmail.com>
2023-08-10 07:57:36 +0200

    fixed randomisation

--- readme.md
+++ readme.md
@@ -2,10 +2,9 @@
 ## gmaeplay
 You have buttons with numbers on it, and you have to find three buttons with,
 and you have to  click them. If you made it to get three buttons with the same 
-number you get 10 points but if not you loose 3 points. Then the buttons disappear and the buttons above will be shown.
+number you get 3 points but if not you loose 1 point. Then the buttons disappear and the buttons above will be shown.
 <br>
-The goal is to get all buttons disappeared, but it is possible that it's not possible
-because the numbers are generated randomly.
+The goal is to get all buttons disappeared.
 <br>
 You have to start the game in the console if you want to see your points they will be just printed into it.
 
--- src/main/kotlin/LFrame.kt
+++ src/main/kotlin/LFrame.kt
@@ -12,23 +12,25 @@ class LFrame : JFrame() {
         defaultCloseOperation = EXIT_ON_CLOSE
         title = "Little Dice Game"
         val screenSize = Toolkit.getDefaultToolkit().screenSize
-        location = Point((screenSize.width - gameTableSize.width) / 2, (screenSize.height - gameTableSize.height - navigatorSize.height) / 2)
+        location = Point(
+            (screenSize.width - gameTableSize.width) / 2,
+            (screenSize.height - gameTableSize.height - navigatorSize.height) / 2
+        )
 
         val gamePanel = JPanel()
         gamePanel.preferredSize = gameTableSize
         gamePanel.layout = null
-        for (y in 0..<gameTableSize.height / (gameTableSize.height / btnCount)){
-            for (x in 0 ..< gameTableSize.width  / (gameTableSize.width / btnCount)){
-                val rnd = Random()
+        for (y in 0..<gameTableSize.height / (gameTableSize.height / btnCount)) {
+            for (x in 0..<gameTableSize.width / (gameTableSize.width / btnCount)) {
                 val posLin = y * 600 / (gameTableSize.width / btnCount) + x
-                btnArray.add(LButton(rnd.nextInt(6) + 1 ,posLin, true))
-                btnArray[posLin].size = Dimension((gameTableSize.width / btnCount),(gameTableSize.width / btnCount))
-                btnArray[posLin].location = Point(x * (gameTableSize.width / btnCount), y * (gameTableSize.width / btnCount))
-//                btnArray[posLin].text = "$y, $x"
+                btnArray.add(LButton(x % 6 + 1, posLin, true))
+                btnArray[posLin].size = Dimension((gameTableSize.width / btnCount), (gameTableSize.width / btnCount))
+                btnArray[posLin].location =
+                    Point(x * (gameTableSize.width / btnCount), y * (gameTableSize.width / btnCount))
 
-                btnArray[posLin].addActionListener{
-                    if(btnArray[posLin].covered != null && !pause){
-                        if(!btnArray[posLin].covered!!){
+                btnArray[posLin].addActionListener {
+                    if (btnArray[posLin].covered != null && !pause) {
+                        if (!btnArray[posLin].covered!!) {
                             btnArray[posLin].covered = null
                             btnArray[posLin].text = ""
 
@@ -40,7 +42,7 @@ class LFrame : JFrame() {
                             actionCounter++
                         }
                     }
-                    if(actionCounter >= 3){
+                    if (actionCounter >= 3) {
                         val t = Timer(500) {
                             endRound()
                             pause = false
@@ -53,15 +55,16 @@ class LFrame : JFrame() {
                 gamePanel.add(btnArray[posLin])
             }
         }
+        randomizePanel()
         repaintPanel()
 
 
         val navigatorPanel = JPanel()
         navigatorPanel.preferredSize = navigatorSize
         navigatorPanel.layout = null
-        for(i in selectBtn){
-            i.size = Dimension(200 / 2,200 / 2)
-            i.location = Point(selectBtn.indexOf(i) * 200 + 50 ,50)
+        for (i in selectBtn) {
+            i.size = Dimension(200 / 2, 200 / 2)
+            i.location = Point(selectBtn.indexOf(i) * 200 + 50, 50)
 
             navigatorPanel.add(i)
         }
@@ -74,8 +77,8 @@ class LFrame : JFrame() {
         isVisible = true
     }
 
-    fun repaintPanel(){
-        for (i in btnArray){
+    fun repaintPanel() {
+        for (i in btnArray) {
             var y = i.position / btnCount
             val x = i.position % btnCount
             y++
@@ -83,28 +86,35 @@ class LFrame : JFrame() {
             val posLin = if (y < btnCount) y * 600 / (gameTableSize.width / btnCount) + x else -1
 
             var lastbtn = true
-            if(posLin != -1){
-                if(btnArray[posLin].covered != null){
+            if (posLin != -1) {
+                if (btnArray[posLin].covered != null) {
                     lastbtn = false
                 }
             }
-            if(posLin == -1 || lastbtn){
-                i.text = if(i.covered != null) {i.diceValue.toString()} else {""}
-                i.covered = if(i.covered != null) {false} else {null}
+            if (posLin == -1 || lastbtn) {
+                i.text = if (i.covered != null) {
+                    i.diceValue.toString()
+                } else {
+                    ""
+                }
+                i.covered = if (i.covered != null) {
+                    false
+                } else {
+                    null
+                }
             }
 
         }
     }
 
-    fun endRound(){
+    fun endRound() {
         actionCounter = 0
 
-        if(selectBtn[0].text == selectBtn[1].text && selectBtn[1].text == selectBtn[2].text) {
-            points += 10
+        if (selectBtn[0].text == selectBtn[1].text && selectBtn[1].text == selectBtn[2].text) {
+            points += 3
             println("your points: $points")
-        }
-        else{
-            for (i in selectedBtn.reversed()){
+        } else {
+            for (i in selectedBtn.reversed()) {
                 var posLin = i.position
                 btnArray[posLin].text = i.diceValue.toString()
                 btnArray[posLin].covered = false
@@ -114,29 +124,41 @@ class LFrame : JFrame() {
                 y--
 
                 posLin = if (y >= 0) y * 600 / (gameTableSize.width / btnCount) + x else -1
-                if(posLin != -1){
+                if (posLin != -1) {
                     btnArray[posLin].text = ""
                     btnArray[posLin].covered = true
                 }
             }
 
-            points -= 3
+            points -= 1
             println("your points: $points")
         }
-        for (i in selectBtn){
+        for (i in selectBtn) {
             i.text = ""
         }
         selectedBtn.clear()
 
         var allFinished = true
-        for (i in btnArray){
-            if(i.covered != null){
+        for (i in btnArray) {
+            if (i.covered != null) {
                 allFinished = false
             }
         }
-        if(allFinished){
+        if (allFinished) {
             dispose()
             println("You have won with $points points!")
+        }
+    }
+
+    fun randomizePanel() {
+        for (i in 0..400) {
+            val rnd = Random()
+            val index1 = rnd.nextInt(btnArray.size - 1)
+            val index2 = rnd.nextInt(btnArray.size - 1)
+
+            val diceValue1 = btnArray[index1].diceValue
+            btnArray[index1].diceValue = btnArray[index2].diceValue
+            btnArray[index2].diceValue = diceValue1
         }
     }
 }
